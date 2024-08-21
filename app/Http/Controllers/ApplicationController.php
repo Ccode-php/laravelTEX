f1ce176d2b53a94ce1971b1d232196cecfa8b0fb
Ccode-php <ccodephp@gmail.com>
2024-08-21 23:44:59 +0500

    Arza formasi

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Application;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ApplicationController extends Controller
 {
-    //
+    public function store(Request $request){
+
+        if($request->hasFile('file_url')){
+            $name = date('h-m-s') .$request->file('file_url')->getClientOriginalName();
+            $path = $request->file('file_url')->storeAs('public/files', $name);
+        }
+
+         $request->validate([
+            'subject' => 'required|max:255',
+            'message' => 'required',
+            'file_url' =>  'file|mimes:png,jpg,bmp,png,pdf'
+        ]);
+
+        $application = Application::create([
+            'subject' => $request->subject,
+            'message' => $request->message,
+            'file_url' => $path ?? null,
+            'user_id' => auth()->user()->id,
+            
+        ]);
+
+        return redirect()->back();
+    }
 }
